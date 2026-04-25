ddde717347205a37638c9d1edf9c2d0ea8a96b5b
dmanh-ai <ducmanh.isg@gmail.com>
2026-04-25 11:14:59 +0700

    Update server.py

--- server.py
+++ server.py
@@ -34,13 +34,21 @@ if not TOKEN:
 BASE_HEADERS = {
     "u268359": TOKEN,
     "Accept": "application/json, text/plain, */*",
-    "Accept-Language": "vi,en;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "vi-VN,vi;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Connection": "keep-alive",
     "Origin": "https://fiintrade.vn",
     "Referer": "https://fiintrade.vn/",
+    "Sec-Fetch-Dest": "empty",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Site": "same-site",
     "User-Agent": (
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-        "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
+        "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/147.0.0.0 Safari/537.36"
     ),
+    "sec-ch-ua": '"Google Chrome";v="147", "Not.A/Brand";v="8", "Chromium";v="147"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
 }
 
 HOSTS = {
